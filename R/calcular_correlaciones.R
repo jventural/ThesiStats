7095ab15cac691de04af0076596d2d706539cf6d
jventural <jventuraleon@gmail.com>
2024-10-26 07:09:19 -0500

    update_cor

--- R/calcular_correlaciones.R
+++ R/calcular_correlaciones.R
@@ -1,4 +1,4 @@
-calcular_correlaciones <- function(data, columna_inicial, columna_final, method = c("spearman", "pearson"), winsorize = FALSE) {
+calcular_correlaciones <- function(data, columna_inicial, columna_final, method = c("spearman", "pearson"), winsorize = FALSE, show_pval = FALSE) {
 
   # Función para instalar y cargar librerías
   install_and_load <- function(package) {
@@ -19,26 +19,41 @@ calcular_correlaciones <- function(data, columna_inicial, columna_final, method 
   column_range <- which(names(data) %in% c(columna_inicial, columna_final))
   data <- data[, min(column_range):max(column_range)]
 
-  if (winsorize && method == "pearson") {
-    # Calcular la correlación de Pearson winsorizado
-    result <- WRS2::winall(data, tr = 0.2)$cor %>%
-      round(2) %>%
-      diag_aba_na() %>%
-      as.data.frame()
-  } else {
-    # Calcular la correlación normal
-    result <- data %>%
-      cor(method = method) %>%
-      round(2) %>%
-      diag_aba_na() %>%
-      as.data.frame()
+  # Función auxiliar para agregar asteriscos en función del p-valor
+  add_significance <- function(cor_values, p_values) {
+    significance <- ifelse(p_values > 0.05, "ns",
+                           ifelse(p_values <= 0.05 & p_values > 0.01, "*",
+                                  ifelse(p_values <= 0.01 & p_values > 0.001, "**",
+                                         ifelse(p_values <= 0.001 & p_values > 0.0001, "***", "****"))))
+    cor_with_significance <- paste0(format(cor_values, nsmall = 2), significance)
+    return(matrix(cor_with_significance, nrow = nrow(cor_values), dimnames = dimnames(cor_values)))
   }
 
-  return(result)
-}
+  # Calcular la correlación y p-valores
+  if (winsorize && method == "pearson") {
+    win_results <- WRS2::winall(data, tr = 0.2)
+    cor_values <- round(win_results$cor, 2)
+    p_values <- format(win_results$p.values, scientific = FALSE, digits = 4) # Evitar notación científica
+  } else {
+    cor_values <- round(cor(data, method = method), 2)
+    p_values <- NULL
+  }
 
-# Función auxiliar para manejar NA en la diagonal
-diag_aba_na <- function(mat) {
-  diag(mat) <- NA
-  return(mat)
+  # Aplicar significancia si `show_pval` es TRUE
+  if (show_pval && !is.null(p_values)) {
+    result_cor <- add_significance(cor_values, p_values)
+  } else {
+    result_cor <- cor_values
+  }
+
+  # Convertir la diagonal principal en NA
+  result_cor <- diag_aba_na(result_cor)
+
+  # Convertir p-values a NA en la diagonal, si existen
+  if (!is.null(p_values)) {
+    p_values <- diag_aba_na(p_values)
+  }
+
+  # Retornar resultados como lista
+  return(list(correlation = as.data.frame(result_cor), p_values = as.data.frame(p_values)))
 }
