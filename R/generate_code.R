99d1e838e880caf635f3dd35ced5afebdf253c00
jventural <jventuraleon@gmail.com>
2025-03-26 02:45:31 -0500

    update

--- R/generate_code.R
+++ R/generate_code.R
@@ -1,14 +1,14 @@
-generate_code <- function(text, name = "Data") {
+generate_code <- function(text, name = "df_new_renombrado") {
   # Dividir el texto en líneas
   lines <- unlist(strsplit(text, "\n"))
 
-  # Extraer factores e items
+  # Extraer factores e ítems
   factors <- gsub(":.+", "", lines)
   items <- gsub(".+:", "", lines)
   items <- lapply(items, function(x) unlist(strsplit(trimws(x), ", ")))
 
   # Crear el código
-  code <- paste0(name, " %>% \n  rowwise() %>% \n  mutate(")
+  code <- paste0(name, " <- ", name, " %>% \n  rowwise() %>% \n  mutate(")
 
   for (i in seq_along(factors)) {
     factor <- factors[i]
@@ -19,6 +19,7 @@ generate_code <- function(text, name = "Data") {
     code <- paste0(code, factor, " = sum(c_across(c(", item_list, "))),\n         ")
   }
 
+  # Ajuste final para el cierre del mutate
   code <- paste0(substr(code, 1, nchar(code) - 11), ") %>% \n  ungroup()")
 
   return(code)
