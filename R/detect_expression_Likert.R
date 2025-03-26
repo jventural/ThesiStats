b961ebd54cfc0db896687026f0cb1d7c16258e05
jventural <jventuraleon@gmail.com>
2025-03-26 00:22:54 -0500

    update

--- R/detect_expression_Likert.R
+++ R/detect_expression_Likert.R
@@ -1,117 +1,53 @@
-detect_expression_Likert <- function(df, start_zero = TRUE) {
+detect_expression_Likert <- function(df, start_zero = TRUE,
+                                     likert_levels = c("Completamente en desacuerdo", "En desacuerdo", "Me es indiferente", "De acuerdo", "Completamente de acuerdo")) {
+
   library(dplyr)
   library(tidyr)
   library(stringr)
 
-
-  # Función para detectar y ordenar expresiones Likert
-  detect_Likert2 <- function(df) {
-    # Definir las respuestas de Likert y sus puntuaciones
-    likert_responses <- data.frame(
-      normalized_expression = c(
-        # Expresiones del ejemplo de las nuevas imágenes con numeración
-        "moderadamente problematico", "muy problematico", "nada problematico", "solo ligeramente problematico",
-        "bastante buena", "bastante mala", "muy buena", "muy mala",
-
-        # Expresiones anteriores
-        "nunca se queda dormido", "escasa probabilidad de quedarse dormido",
-        "moderada probabilidad de quedarse dormido", "alta probabilidad de quedarse dormido",
-        "ninguna vez en el ultimo mes", "menos de una vez a la semana",
-        "una o dos veces a la semana", "tres o mas veces a la semana",
-        "casi nunca", "casi siempre", "con frecuencia", "en ocasiones", "nunca", "siempre",
-        "completamente de acuerdo", "completamente en desacuerdo", "de acuerdo",
-        "en desacuerdo", "me es indiferente",
-        "completamente falso de mi", "la mayor parte falso de mi", "ligeramente mas verdadero que falso",
-        "moderadamente verdadero de mi", "la mayor parte verdadero de mi", "me describe perfectamente",
-
-        # Expresiones clásicas de Likert
-        "totalmente de acuerdo", "de acuerdo", "indiferente", "en desacuerdo", "totalmente en desacuerdo",
-        "definitivamente si", "probablemente si", "indeciso", "probablemente no", "definitivamente no",
-
-        # Expresiones para frecuencia
-        "nunca", "a veces", "con frecuencia", "muchísimas veces", "siempre",
-
-        # Otros patrones Likert existentes
-        "nunca", "rara vez", "algunas veces", "casi siempre", "siempre",
-        "la mayoria de las veces si", "algunas veces si", "algunas veces no", "la mayoria de las veces no",
-        "completamente verdadero", "verdadero", "ni falso ni verdadero", "falso", "completamente falso",
-        "para nada", "varios dias", "mas de la mitad los dias", "casi todos los dias"
-      ),
-
-      # Puntaje correspondiente para cada expresión de menor a mayor intensidad
-      score = c(
-        # Puntajes asignados a las nuevas expresiones de las imágenes (0 a 3)
-        2, 3, 0, 1,  # Para la imagen de CS12 (problemático)
-        2, 3, 0, 1,  # Para la imagen de CS13 (buena/mala)
-
-        # Puntajes para el resto de expresiones
-        0, 1, 2, 3,
-        0, 1, 2, 3,
-        1, 5, 3, 2, 0, 4,
-        5, 1, 4, 2, 3,
-        1, 2, 3, 4, 5, 6,
-        5, 4, 3, 2, 1,
-        5, 4, 3, 2, 1,
-        0, 1, 2, 3, 4,
-        0, 1, 2, 3, 4,
-        3, 2, 1, 0,
-        5, 4, 3, 2, 1,
-        0, 1, 2, 3
-      )
-    )
-
-    # Eliminar duplicados en likert_responses para evitar conflictos
-    likert_responses <- likert_responses %>%
-      distinct(normalized_expression, .keep_all = TRUE)
-
-    # Normalizar las expresiones para comparación
-    normalize_expression <- function(expression) {
-      expression %>%
-        str_to_lower() %>%
-        str_replace_all("á", "a") %>%
-        str_replace_all("é", "e") %>%
-        str_replace_all("í", "i") %>%
-        str_replace_all("ó", "o") %>%
-        str_replace_all("ú", "u") %>%
-        str_replace_all("ü", "u") %>%
-        str_trim()  # Eliminar espacios en blanco al principio y al final
-    }
-
-    likert_responses$normalized_expression <- sapply(likert_responses$normalized_expression, normalize_expression)
-
-    # Preparar los datos, manteniendo la forma original y creando una versión normalizada para la comparación
-    df <- df %>%
-      pivot_longer(everything(), names_to = "column", values_to = "original_expression") %>%
-      mutate(normalized_expression = normalize_expression(original_expression)) %>%
-      distinct(column, normalized_expression, .keep_all = TRUE)
-
-    # Unir con el marco de datos de respuestas Likert para asignar puntuaciones y mantener el orden según la puntuación
-    combined <- df %>%
-      left_join(likert_responses, by = "normalized_expression") %>%
-      filter(!is.na(score)) %>%
-      select(original_expression, score) %>%
-      distinct() %>%
-      arrange(score)  # Ordenar en función de la puntuación de manera ascendente
-
-    # Devolver las expresiones en su forma original con el orden de las puntuaciones y sus valores numéricos
-    combined$score <- NULL  # Eliminar la columna de puntuación
-    tibble(Alternativas = combined$original_expression)
+  # Función para normalizar las expresiones
+  normalize_expression <- function(expr) {
+    expr %>%
+      str_to_lower() %>%
+      str_replace_all("á", "a") %>%
+      str_replace_all("é", "e") %>%
+      str_replace_all("í", "i") %>%
+      str_replace_all("ó", "o") %>%
+      str_replace_all("ú", "u") %>%
+      str_replace_all("ü", "u") %>%
+      str_trim()
   }
 
-  # Función para agregar la secuencia de puntajes si se requiere
-  agregar_secuencia <- function(df, start_zero = TRUE) {
-    if (start_zero) {
-      df <- df %>%
-        mutate(score = row_number() - 1)
-    } else {
-      df <- df %>%
-        mutate(score = row_number())
-    }
-    return(df)
+  # Normalizar los niveles de respuesta
+  likert_levels_normalized <- sapply(likert_levels, normalize_expression)
+
+  # Convertir el data frame a formato largo y filtrar las respuestas que coincidan
+  df_long <- df %>%
+    pivot_longer(everything(), names_to = "column", values_to = "original_expression") %>%
+    mutate(normalized_expression = normalize_expression(original_expression)) %>%
+    filter(normalized_expression %in% likert_levels_normalized)
+
+  # Verificar si faltan niveles de respuesta en los datos
+  missing_levels <- likert_levels[!(likert_levels_normalized %in% df_long$normalized_expression)]
+  if(length(missing_levels) > 0) {
+    warning("Los siguientes niveles Likert no se encontraron en la data: ", paste(missing_levels, collapse = ", "))
   }
 
-  # Aplicar las funciones a las expresiones detectadas
-  expresiones_ordenadas_df <- detect_Likert2(df)
-  expresiones_ordenadas_df <- agregar_secuencia(expresiones_ordenadas_df, start_zero)
-  return(expresiones_ordenadas_df)
+  # Convertir las respuestas a factor usando los niveles predefinidos
+  df_long <- df_long %>%
+    mutate(Alternativas = factor(original_expression, levels = likert_levels))
+
+  # Extraer las alternativas únicas (sin duplicados)
+  df_unique <- df_long %>%
+    distinct(Alternativas, .keep_all = TRUE) %>%
+    arrange(Alternativas)
+
+  # Asignar la secuencia de puntajes
+  if (start_zero) {
+    df_unique <- df_unique %>% mutate(score = row_number() - 1)
+  } else {
+    df_unique <- df_unique %>% mutate(score = row_number())
+  }
+
+  df_unique %>% select(Alternativas, score)
 }
